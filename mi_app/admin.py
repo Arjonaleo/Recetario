29a849b06b05aaccae50206a5b5be5334e8b6ce6
Arjonaleo <gu23ia0002@globaluniversity.edu.mx>
2025-08-05 20:29:38 -0600

    clase 5 agosto

--- mi_app/admin.py
+++ mi_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Usuario, Vehiculo
+
 
 # Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "fecha_nacimiento")
+    search_fields = ("nombre", )
+    list_filter = ("fecha_nacimiento", )
+
+
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "fecha_compra", "usuario")
--- mi_app/models.py
+++ mi_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre}"
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+
+class Vehiculo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    fecha_compra = models.DateField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Vehiculo"
+        verbose_name_plural = "Vehiculos"
