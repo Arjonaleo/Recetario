68296d9b82891caf84e36c6d06b4f69239331cfd
Arjonaleo <gu23ia0002@globaluniversity.edu.mx>
2025-08-07 20:40:58 -0600

    clase 7 de agosto

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views import View
+import json
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+from django.shortcuts import render 
+from .models import Cliente
 
 # Create your views here.
+class ClienteView (View):
+    @method_decorator(csrf_exempt, name="dispatch")
+    def get(self, request):
+        Clientes = Cliente.objects.all().values()
+        return JsonResponse(list(Clientes), safe=False)
+    
+    def post(self, request):
+        try:
+            data = json.load(request.body)
+            cliente = cliente.objects.create(
+                id = data["id"],
+                nombre = data["nombre"],
+                fecha_nacimiento = data["fecha de nacimiento"],
+            )
+            return JsonResponse({"mensaje": "Cliente creado", "id":cliente.nombre})
+        except:
+            return HttpResponseBadRequest({"error": "Formato invalido"})
+
+
+    """def get (self, request):
+        data = {"respuesta": "Hola mundo"
+                }
+        return JsonResponse(data)
+def post(self, request):
+    try:
+        body = json.loads(request.body)
+    except: 
+        return HttpResponseBadRequest("formato invalido")
+    
+    data = {
+        "status": "ok"
+    }
+    
+    return JsonResponse(data) 
+    """      
+
+class PaginitaView(View):
+    def get (self, request):
+        return render(request, "mi app/index.html")
+    
--- proyecto_3erparcial/settings.py
+++ proyecto_3erparcial/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'proyecto_3erparcial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
