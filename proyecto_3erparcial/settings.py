cd2d94b93bbdcacd8953257cfa368991582eb6cb
Arjonaleo <gu23ia0002@globaluniversity.edu.mx>
2025-08-11 11:18:24 -0600

    resolucion de problemas con git y Adaptar settings.py para usar SQLite y personalizar admin al catálogo de recetas, Actualizar urls.py con rutas para lista y detalle de recetas

--- proyecto_3erparcial/settings.py
+++ proyecto_3erparcial/settings.py
@@ -1,10 +1,7 @@
 import os
 import sys
 from pathlib import Path
-
 from dotenv import load_dotenv
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +22,9 @@ TEST_HEADLESS = os.getenv("TEST_HEADLESS", "False") == "True"
 print(f"DEBUG: {DEBUG}")
 print(f"HOST: {HOST}")
 
-
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,8 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
-     # Local apps
-    "mi_app",
+    "mi_app",  # Tu app para recetas
     # Installed apps
     "jazzmin",
 ]
@@ -60,7 +53,7 @@ ROOT_URLCONF = 'proyecto_3erparcial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "templates"],  # Carpeta para templates HTML
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,45 +67,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'proyecto_3erparcial.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-IS_TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
-
-if IS_TESTING:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "testing.sqlite3"),
-        }
+# Database - Usamos SQLite para simplicidad
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Archivo local de DB
     }
-else:
+}
 
-    options = {}
-    if os.environ.get("DB_ENGINE") == "django.db.backends.mysql":
-        options = {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
-        }
-
-    DATABASES = {
-        "default": {
-            "ENGINE": os.environ.get("DB_ENGINE"),
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
-            "OPTIONS": options,
-        }
-    }
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
+# Password validation (dejamos por defecto)
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -128,147 +91,49 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'es-mx'
-
 TIME_ZONE = 'America/Mexico_City'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+# Static files
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Jazzmin (layout template) settings
+# Jazzmin settings (para admin bonito, lo mantenemos simple)
 JAZZMIN_SETTINGS = {
-    # Text
-    "site_title": "Nombre Genérico",
-    "site_header": "Admin",
-    "site_brand": "Nombre Genérico",
-    "welcome_sign": "Bienvenido a Nombre Genérico",
-    "copyright": "Hecho por mí",
-
-    # Media
-    "site_logo": "core/imgs/favicon.ico",
-    "login_logo": "core/imgs/logo.webp",
-    "login_logo_dark": "core/imgs/logo.webp",
-    "site_logo_classes": "",
-    "site_icon": "core/imgs/favicon.ico",
-    
-    # Search model in header
+    "site_title": "Catálogo de Recetas",
+    "site_header": "Admin Recetas",
+    "site_brand": "Recetario de Cocina",
+    "welcome_sign": "Bienvenido al Admin de Recetas",
+    "copyright": "Hecho por un estudiante principiante",
+    "site_logo": None,  # Puedes agregar un logo después
     "search_model": [],
-
-    # Field name on user model that contains avatar
-    # ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
-
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
-    "topmenu_links": [
-        # {"name": "Landing", "url": LANDING_HOST},
-    ],
-
-    #############
-    # User Menu #
-    #############
-
-    # Additional links to include in the user menu on the top right
-    # ("app" url type is not allowed)
-    "usermenu_links": [
-        # {"model": "auth.user"}
-    ],
-
-    #############
-    # Side Menu #
-    #############
-
-    # Whether to display the side menu
+    "topmenu_links": [],
+    "usermenu_links": [],
     "show_sidebar": True,
-
-    # Whether to aut expand the menu
     "navigation_expanded": True,
-
-    # Hide these apps when generating side menu e.g (auth)
     "hide_apps": [],
-
-    # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [
-    ],
-
-    # List of apps (and/or models) to base side menu ordering off of
-    # (does not need to contain all apps/models)
+    "hide_models": [],
     "order_with_respect_to": [],
-
-    # Custom links to append to app groups, keyed on app name
-    "custom_links": {
-        # "books": [{
-        #     "name": "Make Messages",
-        #     "url": "make_messages",
-        #     "icon": "fas fa-comments",
-        #     "permissions": ["books.view_book"]
-        # }]
-    },
-
-    # Custom icons for side menu apps/models
-    # See https://fontawesome.com/icons?d=gallery&m=free
-    # for the full list of 5.13.0 free icon classes
+    "custom_links": {},
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
     },
-    # Icons that are used when one is not manually specified
     "default_icon_parents": "fas fa-chevron-circle-right",
     "default_icon_children": "fas fa-circle",
-
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
     "related_modal_active": False,
-
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
-    "custom_css": "core/css/custom.css",
-    "custom_js": "core/js/custom.js",
-    # Whether to link font from fonts.googleapis.com
-    # (use custom_css to supply font otherwise)
+    "custom_css": None,
+    "custom_js": None,
     "use_google_fonts_cdn": True,
-    # Whether to show the UI customizer on the sidebar
     "show_ui_builder": False,
-
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
     "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
-    # "changeform_format_overrides": {
-    #     "auth.user": "horizontal_tabs",
-    #     "auth.group": "carousel"
-    # },
 }
 
 JAZZMIN_UI_TWEAKS = {
@@ -303,7 +168,7 @@ JAZZMIN_UI_TWEAKS = {
     }
 }
 
-# Cors
+# CORS/CSRF (opcional, lo dejamos si lo necesitas)
 if os.getenv('CORS_ALLOWED_ORIGINS') != 'None':
     CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 
